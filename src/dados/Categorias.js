e93957ff52ae324c267977bb7e8accd6e622dc26
Barbara Sousa <barbarapsousa@hotmail.com>
2021-09-30 06:30:23 -0300

    Criando lista de Inscritos

--- src/dados/Categorias.js
+++ src/dados/Categorias.js
@@ -1,13 +1,20 @@
-
 export default class Categorias {
-
-  constructor(){
-    this.categorias = [];
+  constructor() {
+    this.categorias = []
+    this._inscritos = [] // guarda quem vai ser notificado
   }
+
+  inscrever(fun) {
+    this._inscritos.push(fun)
+  }
+
+  notificar() {
+    this._inscritos.forEach(fun => {
+      fun(this.categorias)
+    })
+  } // para cada inscrito, executa a função que foi passada na categoria
 
   adicionarCategoria(novaCategoria) {
-    console.log(this.categorias);
     this.categorias.push(novaCategoria)
   }
-
 }
--- src/dados/Notas.js
+++ src/dados/Notas.js
@@ -1,15 +1,26 @@
 export default class ArrayDeNotas {
   constructor() {
     this.notas = []
+    this._inscritos = []
   }
 
   adicionarNotas(titulo, texto, categoria) {
-    const novaNota = new Nota(titulo, texto, categoria);
+    const novaNota = new Nota(titulo, texto, categoria)
     this.notas.push(novaNota)
   }
 
   apagarNota(indece) {
     this.notas.splice(indece, 1)
+  }
+
+  inscrever(fun) {
+    this._inscritos.push(fun)
+  }
+
+  notificar() {
+    this._inscritos.forEach(fun => {
+      fun(this.categorias)
+    })
   }
 }
 
